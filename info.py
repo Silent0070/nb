3998f897d23cf00673e26b47255bfd13229db1be
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-12-14 16:38:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,8 +33,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "anurag")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'anuragfiles')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Anurag")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Anurag_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
