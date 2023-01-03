f97cdaf0a07cc9788283a6cd952492e40e7ee2e5
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-03 09:56:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/AutoAnura /AutoAnura
+  git clone https://github.com/TeenOfFire/Cineplix /Cineplix
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoAnura
+  git clone $UPSTREAM_REPO /Cineplix
 fi
-cd /AutoAnura
+cd /Cineplix
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
