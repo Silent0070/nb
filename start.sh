2d003314e01f3b596fba448e6a6a24028e59e26e
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2023-01-04 14:09:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jnanesh124/v7bot /v7bot 
+  git clone https://github.com/Jnanesh124/v7bot  /Cineplix
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /v7bot 
+  git clone $UPSTREAM_REPO /Cineplix
 fi
-cd /v7bot 
+cd /Cineplix
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
