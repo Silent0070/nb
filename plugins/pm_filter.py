49d427143958c3542d88a5f1fcdd8bc03ecbbcdd
Jnanesh124 <99884442+Jnanesh124@users.noreply.github.com>
2023-01-04 12:34:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -62,14 +62,14 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("❗Honey, It's Not For You 🚫 \n❗हनी, ये तुम्हारे लिए नहीं है 🚫", show_alert=True)
+        return await query.answer("It's Not For You", show_alert=True)
     try:
         offset = int(offset)
     except:
         offset = 0
     search = BUTTONS.get(key)
     if not search:
-        await query.answer("🚫Link Expired, Request Again ♻", show_alert=True)
+        await query.answer("Link Expired, Request Again ", show_alert=True)
         return
 
     files, n_offset, total = await get_search_results(search, offset=offset, filter=True)
@@ -149,12 +149,12 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("❗Honey, It's Not For You 🚫 \n❗हनी, ये तुम्हारे लिए नहीं है 🚫", show_alert=True)
+        return await query.answer("Honey, It's Not For You", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.id)
     if not movies:
-        return await query.answer("🚫Link Expired, Request Again ♻", show_alert=True)
+        return await query.answer("Link Expired, Request Again", show_alert=True)
     movie = movies[(int(movie_))]
     await query.answer('Checking in Database ♻️')
     k = await manual_filters(bot, query.message, text=movie)
@@ -224,7 +224,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 except:
                     pass
             else:
-                await query.answer("❗Honey, It's Not For You 🚫 \n❗हनी, ये तुम्हारे लिए नहीं है 🚫", show_alert=True)
+                await query.answer("It's Not For You", show_alert=True)
     elif "groupcb" in query.data:
         await query.answer()
 
